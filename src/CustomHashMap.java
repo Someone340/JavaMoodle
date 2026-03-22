a4ffb6c8fc0241c61376fe8a4b484285ddad8926
Someone340 <oplusr74@gmail.com>
2026-03-22 18:50:53 +0300

    Add files via upload

---
+++ src/CustomHashMap.java
@@ -0,0 +1,89 @@
+public class CustomHashMap<K, V> {
+    private static class Node<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Node<K, V> next;
+
+        Node(int hash, K key, V value) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+        }
+
+        public final String toString() {
+            return key + " = " + value;
+        }
+
+        public final boolean equals(Object o) {
+            return o == this;
+        }
+    }
+
+    private Node<K, V>[] buckets;
+    private final int CAPACITY = 16;
+
+    public CustomHashMap() {
+        buckets = new Node[CAPACITY];
+    }
+
+    private int hash(K key) {
+        return (key == null) ? 0 : Math.abs(key.hashCode() % CAPACITY);
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+        Node<K, V> node = buckets[index];
+
+        while (node != null) {
+            if (node.key != null && node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            if (node.next == null) break;
+            node = node.next;
+        }
+
+        Node<K, V> newNode = new Node<>(hash(key), key, value);
+        if (buckets[index] == null) {
+            buckets[index] = newNode;
+        } else {
+            node.next = newNode;
+        }
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+        Node<K, V> node = buckets[index];
+        while (node != null) {
+            if (node.key != null && node.key.equals(key)) return node.value;
+            node = node.next;
+        }
+        return null;
+    }
+
+    public void remove(K key) {
+        int index = hash(key);
+        Node<K, V> node = buckets[index];
+        Node<K, V> prev = null;
+
+        while (node != null) {
+            if (node.key != null && node.key.equals(key)) {
+                if (prev == null) buckets[index] = node.next;
+                else prev.next = node.next;
+                return;
+            }
+            prev = node;
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        CustomHashMap<String, Integer> test = new CustomHashMap<>();
+        test.put("Test1", 1);
+        test.put("Test2", 2);
+        System.out.println(test.get("Test1"));
+        test.remove("Test1");
+        System.out.println(test.get("Test1"));
+    }
+}
